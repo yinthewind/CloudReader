fdfdbacb644b52425b9e8f579b887c9b357266bb
Yinthewind <yinthewater@gmail.com>
2016-06-13 00:07:55 -0700

    remove page.js, add page.jsx, call <PdfViewer/> from there

--- src/page.jsx
+++ src/page.jsx
@@ -1,14 +1,18 @@
-var Viewer = require('./Viewer');
+'use strict'
 
-document.addEventListener('DOMContentLoaded', function() {
+var React = require('react');
+var ReactDOM = require('react-dom');
+var PdfViewer = require('./PdfViewer');
 
-	chrome.runtime.onMessage.addListener(
-		function(request, sender, sendResponse) {
-			if(request.type == 'openPdf') {
-				renderPdf(
-					request.bookUrl, document.getElementById('container'));
-			}
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		
+		if(request.type == 'openPdf') {
+			ReactDOM.render(
+				<PdfViewer url={request.bookUrl} />,
+				document.getElementById('container')
+			);
 		}
-	);
-});
+	}
+);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = [
 	{
 		entry: {
-			page: './src/page.js',
+			page: './src/page.jsx',
 			popup: './src/popup.jsx'
 		},
 		module: {
