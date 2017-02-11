eabc4d2ef3138f6bb2b9623bd7715dfbd54122c0
Yinthewind <yinthewater@gmail.com>
2017-02-11 12:21:33 -0800

    update webpack-dev-server usage

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,19 @@
-module.exports = [
-	{
-		entry: {
-			pageLoader: './src/pageLoader.jsx',
-			popupLoader: './src/popupLoader.jsx'
-		},
-		module: {
-			loaders: [
-				{ test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
-				{ test: /\.css$/, loader: 'style-loader!css-loader'}
-			]
-		},
-		output: {
-			path: __dirname + '/src',
-			filename: '[name].bundle.js'
-		},
-		resolve: {
-			extensions: ['', '.js', '.jsx']
-		}
+module.exports = {
+	entry: {
+		pageLoader: './src/pageLoader.jsx',
+		popupLoader: './src/popupLoader.jsx'
 	},
-	{
-		entry: {
-			testPopup: './dev/testPopup.jsx',
-			testPage: './dev/testPage.jsx'
-		},
-		module: {
-			loaders: [ 
-				{ test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
-				{ test: /\.css$/, loader: 'style-loader!css-loader'}
-			]
-		},
-		output: {
-			path: __dirname + '/dev',
-			filename: '[name].bundle.js',
-			publicPath: 'http://localhost:8091/'
-		},
-		resolve: {
-			extensions: ['', '.js', '.jsx']
-		}
+	module: {
+		loaders: [
+			{ test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
+			{ test: /\.css$/, loader: 'style-loader!css-loader'}
+		]
+	},
+	output: {
+		path: __dirname + '/src',
+		filename: '[name].bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
 	}
-];
+};
