3d0bd055f4e986d635e16897c7cc424f7076ba35
Yinthewind <yinthewater@gmail.com>
2017-02-24 09:50:25 -0800

    add alert icon to notification panel

--- src/NotificationPanel.jsx
+++ src/NotificationPanel.jsx
@@ -1,8 +1,12 @@
 var React = require('react');
+import { IoAlertCircled } from 'react-icons/lib/io';
 
 module.exports = React.createClass({
 	render: function() {
 		if(!this.props.message) return null;
-		return <div className={'notification-panel'}>{this.props.message.toString()}</div>
+		return <div className={'notification-panel'}>
+			<IoAlertCircled className='menu-icon'/>
+			{this.props.message.toString()}
+		</div>
 	}
 });
