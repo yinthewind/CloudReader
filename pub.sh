5ca5c77887b386e6e03bdfa3365f3a5063fc6249
Yinthewind <yinthewater@gmail.com>
2017-02-25 23:47:07 -0800

    pre publish

---
+++ pub.sh
@@ -0,0 +1,9 @@
+cp web/icon.png publish/
+cp web/popup.html publish/
+cp web/page.html publish/
+cp web/pageLoader.bundle.js publish/
+cp web/popupLoader.bundle.js publish/
+cp web/1.1.bundle.js publish/
+cp web/manifest.json publish/
+
+zip -r CloudReader.zip publish/
