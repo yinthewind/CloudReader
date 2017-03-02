2c1bcce367d2941ae513bf37573b4ae2b749ee99
Yinthewind <yinthewater@gmail.com>
2017-03-01 22:24:36 -0800

    add a popup title div explain to users how to upload more pdfs

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CloudReader
 Read your PDFs anywhere anytime, enjoy extremely simple access to all of them and never worry about where you were(automatically sync reading progress)
 
-## CloudReader Demo
+## CloudReader Online Demo
 Click [here](https://yinthewind.github.io/javascript/pdf/reader/2017/02/15/Cloud-Reader.html)
 
 ## Setup
--- src/popup.jsx
+++ src/popup.jsx
@@ -7,6 +7,7 @@ module.exports = React.createClass({
 	render: function() {
 		return (
 			<div className='popup'>
+				<div className='popup-title'>Below are the PDFs in your Google Drive. <a href={'https://drive.google.com/'}>Click here to Upload more PDFs</a> and start reading</div>
 				<BookList books={this.props.files}/>
 			</div>
 		)
